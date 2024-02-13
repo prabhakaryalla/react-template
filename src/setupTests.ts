b509599c657a9b76ecf6350a31492a2da49f8cb8
Prabhakara R Yalla <Prabhakara.Rao.Yalla@gds.ey.com>
2024-02-13 17:41:29 +0530

    Fixing console warning while running tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import chalk from 'chalk';
+import 'jest-canvas-mock';
+
+/*
+ * Capture console errors from libraries
+ */
+const consoleSpy = jest.spyOn(global.console, 'error');
+
+afterEach(done => {
+  if (consoleSpy.mock.calls.length){
+    done.fail(`${chalk.red('console.error')} was called ${chalk.yellow(consoleSpy.mock.calls.length.toString())} times.`);
+  }
+  consoleSpy.mockClear();
+
+  done();
+})
