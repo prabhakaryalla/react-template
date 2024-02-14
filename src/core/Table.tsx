9924af9af317c9b541153f85785d24540f3453ea
YALLA PRABHAKARA RAO <42337157+prabhakaryalla@users.noreply.github.com>
2024-02-14 14:45:37 +0530

    Update Table.tsx

--- src/core/Table.tsx
+++ src/core/Table.tsx
@@ -92,3 +92,170 @@ const localeCompareSort = (col: Column<RowData>) => (data1: RowData, data2: RowD
   
 
 export default Table;
+
+
+// import { useTheme } from "@mui/material";
+// import { MaterialReactTable, MaterialReactTableProps } from "material-react-table";
+
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// type RowData = any;
+
+
+// export const Table = function Table(props: MaterialReactTableProps<RowData>) {
+
+
+//     const theme = useTheme();
+
+
+//     const defaultProps: Partial<MaterialReactTableProps<RowData>> = {
+//         enableGlobalFilter: true,
+//         enableFullScreenToggle: false,
+//         state: {
+//             density: 'compact'
+//         },
+//         positionActionsColumn: 'last',
+//         enableStickyHeader: true,
+
+
+//         muiTopToolbarProps: {
+//             sx: {
+//                  background : theme.palette.primary.main
+//             }
+//         },
+
+
+//         muiSearchTextFieldProps: {
+//             variant: "outlined",
+//             sx: {
+//                 minWidth: '18rem',
+//             }
+//         },
+//         muiTablePaperProps: {
+//             elevation: 0,
+//             sx: {
+//                 borderRadius: '0',
+//             },
+//         },
+//         muiTableBodyProps: {
+//             sx: {
+//                 //stripe the rows, make odd rows a darker color
+//                 '& tr:nth-of-type(odd) > td': {
+//                     backgroundColor: '#f5f5f5',
+//                 },
+//             },
+//         },
+//     };
+
+
+//     const { ...tableProps } = { ...defaultProps, ...props };       // Shallow merge props
+//     tableProps.state = { ...defaultProps.state, ...props.state };                                // Merge options
+
+
+//     return (<MaterialReactTable  {...tableProps as MaterialReactTableProps<RowData>} />);
+
+
+// }
+
+
+// export default Table;
+
+
+
+// import { Edit, Add } from "@mui/icons-material";
+// import { Box, Checkbox, Fab, IconButton, Tooltip } from "@mui/material";
+// import { NavLink, useNavigate } from "react-router-dom";
+// import FullPage from "../../layout/FullPage";
+// import { useEffect, useMemo, useState } from "react";
+// import { useCompanyContext } from "./CompanyContext";
+// import ICompanyModel from "./ICompanyModel";
+// import { Table } from '../../core/Table';
+// import CompanyClient from "./CompanyClient";
+// import { MRT_ColumnDef } from "material-react-table";
+
+
+// export const AddLink = () => {
+//     return (
+//         <Fab to="/companies/create" component={NavLink} variant="extended" color="primary" aria-label="add company" size="medium">
+//             <Add />Add Company
+//         </Fab>);
+// }
+
+
+// // const columns = [
+// //     { title: 'Company Name', field: 'name' },
+// //     { title: 'Company Code', field: 'code' },
+// //     { title: 'MPT Short Name', field: 'mptshortname' },
+// //     { title: 'Wire Company', field: 'type' },
+// //     { title: 'Exceptions Allowed', field: 'exceptionallowed', render: (rowData: any) => <Checkbox style={{ height: "5px" }} checked={rowData.exceptionallowed} /> }
+// // ];
+
+
+// const CompanyList = () => {
+
+
+//     const columns = useMemo<MRT_ColumnDef<ICompanyModel>[]>(
+//         () => [
+//             {
+//                 accessorKey: 'id', 
+//                 header: 'Id',
+//             },
+//             {
+//                 accessorKey: 'name', 
+//                 header: 'Company Name',
+//                 size: 150,
+//             },
+//             {
+//                 accessorKey: 'code',
+//                 header: 'Code',
+//                 size: 150,
+//             },
+//             {
+//                 accessorKey: 'mptshortname', 
+//                 header: 'MPT Short Name',
+//                 size: 200,
+//             },
+//             {
+//                 accessorKey: 'type',
+//                 header: 'Wire Company',
+//                 size: 150,
+//             }
+//         ],
+//         [],
+//     );
+
+
+//     const [isLoadingCompanies, setIsLoadingCompanies] = useState(true);
+//     const nav = useNavigate();
+//     const { companies, setCompanies } = useCompanyContext();
+
+
+//     useEffect(() => {
+//         setIsLoadingCompanies(true);
+//         CompanyClient.fetchCompanies().then((data) => {
+//             setCompanies((data || []) as ICompanyModel[]);
+//         }).catch((err) => {
+//             console.error(JSON.stringify(err))
+//             setCompanies([] as ICompanyModel[]);
+//         }).finally(() => {
+//             setIsLoadingCompanies(false);
+//         })
+//     }, [])
+
+
+//     return (
+//         <FullPage heading="Autowire Companies" actions={<AddLink />} >
+//             <Table state={{ isLoading: isLoadingCompanies, columnVisibility: { id: false } }} enableRowActions={true} columns={columns} data={companies}
+//                 renderRowActions={({ row  }) => (
+//                     <Box sx={{ display: 'flex', gap: '1rem' }}>
+//                         <Tooltip title="Edit">
+//                             <IconButton onClick={() => nav(`/companies/edit/${row.original.id}`)}>
+//                                 <Edit />
+//                             </IconButton>
+//                         </Tooltip>
+//                     </Box>
+//                 )}
+//             />
+//         </FullPage>
+//     )
+// }
